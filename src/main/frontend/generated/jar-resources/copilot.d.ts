251fbd416b9675e124ae996114297a04ce2638d4
salah <@salah>
2025-08-06 17:59:42 +0330

    first

--- src/main/frontend/generated/jar-resources/copilot.d.ts
+++
@@ -1,3 +0,0 @@
-export { _registerImporter } from './copilot/figma-public/figma-api';
-export type { FigmaNode } from './copilot/figma-public/figma-api';
-export type { ComponentDefinition, ComponentDefinitionProperties } from './copilot/shared/flow-utils';
--- src/main/frontend/generated/jar-resources/copilot.js
+++
@@ -1,4 +0,0 @@
-import { ar as o } from "./copilot/copilot-Dqxg7m7a.js";
-export {
-  o as _registerImporter
-};
